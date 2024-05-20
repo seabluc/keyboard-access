2120bd3e6f373e2345b5766349f6b872ac9b9635
seab <seanluc.ghim@gmail.com>
2024-05-20 15:20:15 -0700

    &apos;

--- src/app/attention/page.js
+++ src/app/attention/page.js
@@ -1,3 +1,4 @@
+// ===========================POC: Guiding Attention===========================
 export default function Attention() {
   return (
     <div className="flex-wrap">
@@ -7,10 +8,10 @@ export default function Attention() {
         <div className="flex min-w-10 min-h-10">
           <img src="./belle-pls.gif" alt="oh nnooo" className="w-52 h-52"/>
           <article className="ml-4 px-2 py-4 border-2">
-            <p className="font-sans text-lg">This is Belle O'Neill, Kansas's Doberman/cattle dog/hound mix rescue dog</p>
+            <p className="font-sans text-lg">This is Belle O&apos;Neill, Kansas&apos;s Doberman/cattle dog/hound mix rescue dog</p>
             <p className="font-sans text-base py-2 text-wrap">She was born with a birth defect that stretches the muscles on her head backwards.</p>
-            <p className="font-sans text-base py-2 text-wrap">As a result, she permanently looks shocked or surprised, but thankfully it's harmless!</p>
-            <p className="font-sans text-base py-2 text-wrap">Belle's Tiktok currently has 800k Followers and a total of 25.8M Likes.</p>
+            <p className="font-sans text-base py-2 text-wrap">As a result, she permanently looks shocked or surprised, but thankfully it&apos;s harmless!</p>
+            <p className="font-sans text-base py-2 text-wrap">Belle&apos;s Tiktok currently has 800k Followers and a total of 25.8M Likes.</p>
             <p className="font-sans text-base py-2 text-wrap">Her beauty blinds me and she truly is the goodest of doggos.</p>
           </article>
         </div>
